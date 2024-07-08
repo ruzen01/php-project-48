73f4901f600fd74f745c3909a0c8959f37b3fcea
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2024-07-08 05:47:45 +0000

    fix: Step 8, yml ext

---
+++ tests/JsonFormatterTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Differ\Formatters\Json;
+
+class JsonFormatterTest extends TestCase
+{
+    public function testJsonFormat()
+    {
+        $diff = [
+            'common' => [
+                'setting1' => 'Value 1',
+                'setting2' => '200',
+                'setting3' => true
+            ],
+            'group1' => [
+                'baz' => 'bas',
+                'foo' => 'bar'
+            ]
+        ];
+
+        $expected = json_encode($diff, JSON_PRETTY_PRINT);
+        $this->assertEquals($expected, Json\format($diff));
+    }
+}
